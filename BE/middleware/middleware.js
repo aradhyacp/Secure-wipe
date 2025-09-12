bb0d62f120f24a9a7e01dc4e2d22b235af19034f
aradhyacp <cparadhya@proton.me>
2025-09-12 23:38:19 +0530

    added middleware for routing

---
+++ BE/middleware/middleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import config from '../config.js';
+
+export function authMiddleware(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'Token missing' });
+    try {
+        const user = jwt.verify(token, config.JWT_SECRET);
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: 'Invalid token' });  
+    }
+}
